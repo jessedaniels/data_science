ccc0e81f957e47b570fd32733ad2b23975d2404f
Jesse Daniels <jessedaniels1@gmail.com>
2014-05-18 15:10:12 -0700

    full backprop implemented

--- neuralnet_testbed.py
+++ neuralnet_testbed.py
@@ -2,36 +2,43 @@
 import numpy as np
 import math
 
-
-data_dim = 7
-hidden_nodes = 5
+hidden_hodes = 5
 output_nodes = 1
+lam = 0.001 # regulation parameter
+alpha = 0.01 # learning rate
+W1 = None
+W2 = None
 
-W1 = np.random.rand(hidden_nodes, data_dim + 1)/50.0
-W2 = np.random.rand(output_nodes, hidden_nodes + 1)/50.0
+def initialize(x):
+    global hidden_nodes, output_nodes, W1, W2
+    data_dim = x.shape[1]
 
-# Activations in first and output layers
-a1 = None
-a2 = None
-
-# Error values for each layer
-delta_1 = None
-delta_2 = None
+    W1 = np.random.rand(hidden_nodes, data_dim + 1)/20.0
+    W2 = np.random.rand(output_nodes, hidden_nodes + 1)/20.0
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def do_pass(x, y):
-    # Add bias and make examples columns; ith training example is x[:,i]
-    x = np.hstack([np.ones((x.shape[0], 1)), x]).T
+    global W1, W2 
 
-    # Activations in hidden and output layers
-    a1 = sigmoid(W1.dot(x))
-    a2 = sigmoid(W2.dot(a1))
+    # Forward pass
+    z2 = W1.dot(np.hstack([np.ones((x.shape[0], 1)), x]).T)  
+    a2 = sigmoid(z2)
+         
+    z3 = W2.dot(np.vstack([np.ones((1, a2.shape[1])), a2]))
+    a3 = sigmoid(z3)
 
     # Backpropagate errors
-    delta_2 = -(y - a2) * a2*(1 - a2)
-    delta_1 = w21.T.dot(d2) * a1*(1-a1)
-    
-    
+    delta3 = -(y - a3) * a3*(1 - a3)
+    # Derivative wrt activations removes bias from W2 at this point, so have to slice it out
+    delta2 = W2[:,1:].T.dot(delta3) * a2*(1 - a2)
 
+    # Derivatives wrt weights adds a row of 1s for the biases since it is a constant weight not multiplied by anything
+    # Dimensions of grad3 and grad2 should equal W2 and W1, respectively
+    grad2 = delta3.dot(np.vstack([np.ones((1,a2.shape[1])), a2]).T)
+    grad1 = delta2.dot(np.hstack([np.ones((x.shape[0], 1)), x])) # x is already the right dimension - no need to transpose
+    
+    # Update the weight arrays
+    W2 -= alpha * (1.0/x.shape[0] * grad2 +  np.hstack([np.zeros([W2.shape[0], 1]), W2[:,1:] * l]))
+    W1 -= alpha * (1.0/x.shape[0] * grad1 +  np.hstack([np.zeros([W1.shape[0], 1]), W1[:,1:] * l]))
