859a2a2b115de8452bc515711ac6b8c9e400250a
Jesse Daniels <jessedaniels1@gmail.com>
2014-05-19 19:47:03 -0700

    trying to to gradient checking. Not working.

--- neuralnet_testbed.py
+++ neuralnet_testbed.py
@@ -36,9 +36,34 @@ def do_pass(x, y):
 
     # Derivatives wrt weights adds a row of 1s for the biases since it is a constant weight not multiplied by anything
     # Dimensions of grad3 and grad2 should equal W2 and W1, respectively
-    grad2 = delta3.dot(np.vstack([np.ones((1,a2.shape[1])), a2]).T)
-    grad1 = delta2.dot(np.hstack([np.ones((x.shape[0], 1)), x])) # x is already the right dimension - no need to transpose
+    grad2 = 1.0 / x.shape[0] * delta3.dot(np.vstack([np.ones((1,a2.shape[1])), a2]).T)
+    grad1 = 1.0 / x.shape[0] * delta2.dot(np.hstack([np.ones((x.shape[0], 1)), x])) # x is already the right dimension - no need to transpose
     
     # Update the weight arrays
-    W2 -= alpha * (1.0/x.shape[0] * grad2 +  np.hstack([np.zeros([W2.shape[0], 1]), W2[:,1:] * l]))
-    W1 -= alpha * (1.0/x.shape[0] * grad1 +  np.hstack([np.zeros([W1.shape[0], 1]), W1[:,1:] * l]))
+    W2 -= alpha * (grad2 +  np.hstack([np.zeros([W2.shape[0], 1]), W2[:,1:] * lam]))
+    W1 -= alpha * (grad1 +  np.hstack([np.zeros([W1.shape[0], 1]), W1[:,1:] * lam]))
+
+
+def main(x, y):
+
+    # Break up x into minibatches
+
+
+# TODO adaptive learning rate?
+
+def gradient_checking(x, y, W1, W2):
+
+    epsilon = 0.0001
+    grad_estimate = (cost_function(x, y, W1 + epsilon, W2 + epsilon) - cost_function(x, y, W1 - epsilon, W2 - epsilon)) / (2 * epsilon)     
+
+def cost_function(x, y, W1, W2):
+    z2 = W1.dot(np.hstack([np.ones((x.shape[0], 1)), x]).T)  
+    a2 = sigmoid(z2)
+         
+    z3 = W2.dot(np.vstack([np.ones((1, a2.shape[1])), a2]))
+    a3 = sigmoid(z3)
+
+    return np.mean(0.5 * (a3 - y) ** 2) + 0.5 * lam * np.sum(W1[:,1:] ** 2)
+
+    
+    
