e21c143952a26adae056e5676a311b9c1afe9721
Jesse Daniels <jessedaniels1@gmail.com>
2014-05-20 19:53:49 -0700

    updates

--- neuralnet_testbed.py
+++ neuralnet_testbed.py
@@ -2,39 +2,48 @@
 import numpy as np
 import math
 
-hidden_nodes = 50
-output_nodes = 10
-lam = 0.001 # regulation parameter
+hidden_nodes = 5
+output_nodes = 1
+lam = 0.01 # regulation parameter
 alpha = 0.01 # learning rate
 W1 = None
 W2 = None
 
 def initialize(x):
-    global hidden_nodes, output_nodes, W1, W2
+    global W1 
+    global W2
+
     data_dim = x.shape[1]
 
     W1 = np.random.rand(hidden_nodes, data_dim + 1)/20.0
     W2 = np.random.rand(output_nodes, hidden_nodes + 1)/20.0
+    
+    return W1, W2
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 # Accepts a minibatch of (x,y) vectors to compute gradients and update weights.
-def do_pass(x, y):
-    global W1, W2 
-    
+def do_pass(x, y, alpha, lam):
+    global W1
+    global W2
+
+    ### Forward Pass ###
+
     # Add ones column to x for bias
+    # TODO: Inefficient use of memory to keep these kinds of matrices around (with and without bias) - x and a2?
     x_w_b = np.hstack([np.ones((x.shape[0], 1)), x])
-    # Forward pass
+    
     z2 = W1.dot(x_w_b.T)
     a2 = sigmoid(z2)
     
     # Add ones column to a2 for bias
     a2_w_b = np.vstack([np.ones((1, a2.shape[1])), a2])
     z3 = W2.dot(a2_w_b)
-    a3 = sigmoid(z3)
+    #a3 = sigmoid(z3)
+    a3 = z3 # Linear output unit
 
-    # Backpropagate errors
+    ### Backpropagate errors ###
     delta3 = -(y - a3) * a3*(1 - a3)
     # Derivative wrt activations removes bias from W2 at this point, so have to slice it out
     delta2 = W2[:,1:].T.dot(delta3) * a2*(1 - a2)
@@ -51,23 +60,39 @@ def do_pass(x, y):
     # Update the weight arrays
     W2 -= alpha * grad2
     W1 -= alpha * grad1
-    
-    print np.mean(0.5 * (y - a3) ** 2)
+   
+    # TODO set learning rate based on epoch
+
+    cost = np.mean(0.5 * (y - a3) ** 2)
+    print alpha, lam, cost
+    return cost
 
 def main(x, y):
-
+    pass
     # Break up x into minibatches
 
 
-# TODO adaptive learning rate?
+def predict(x, W1, W2):
+    x_w_b = np.hstack([np.ones((x.shape[0], 1)), x])
+    
+    z2 = W1.dot(x_w_b.T)
+    a2 = sigmoid(z2)
+    
+    # Add ones column to a2 for bias
+    a2_w_b = np.vstack([np.ones((1, a2.shape[1])), a2])
+    z3 = W2.dot(a2_w_b)
+    a3 = sigmoid(z3)
+    
+    return a3
+
 
 def gradient_checking(x, y, W1, W2):
     # Note for gradient checking - make sure the regularization derivatives are incorporated into the gradient computed above before comparing.
     # It might be good to create a version that accepts an unrolled parameter vector to make gradient checking easier. I spot checked it and it looked good. 
     epsilon = 0.0001
-    t = np.zeros_like(W1)
-    t[4,10] = epsilon
-    grad_estimate = (cost_function(x, y, W1+t, W2) - cost_function(x, y, W1-t, W2)) / (2 * epsilon)     
+    t = np.zeros_like(W2)
+    t[0,10] = epsilon
+    grad_estimate = (cost_function(x, y, W1, W2+t) - cost_function(x, y, W1, W2-t)) / (2 * epsilon)     
 
 
 def cost_function(x, y, W1, W2):
