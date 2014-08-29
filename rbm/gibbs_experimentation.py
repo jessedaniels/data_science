a36c3f77f80b026f2a3204dc922837c24cace688
Jesse Daniels <jessedaniels1@gmail.com>
2014-08-28 19:48:28 -0700

    Initial re-arrangement of directories and added rbm

---
+++ rbm/gibbs_experimentation.py
@@ -0,0 +1,7 @@
+
+
+# Given a binary visible vector:
+# 1) Determine probabilities that hidden units are on
+# 1a) Need the weight matrices. Absorb the biases into the weight matrix. Will need to sit down and spec out the matricies to make sure everything is set up correctly.
+# 2) Do thresholding of hidden units based on thresholding of a uniform random number
+
